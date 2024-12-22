0f7d902a60f2333f607c31a9977c1c32dfa01ce5
Ivan Hurov <ivan.hurov@nure.ua>
2024-12-22 17:56:45 +0300

    Add files via upload

--- Task2/arkpz-pzpi-22-8-hurov-ivan-task2/server.js
+++ Task2/arkpz-pzpi-22-8-hurov-ivan-task2/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 const userRoutes = require('./routes/userRoutes');
+const swaggerDocs = require('./config/swagger');
+
 
 dotenv.config();
 connectDB();
@@ -9,6 +11,9 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use('/api', userRoutes);
+
+// Swagger
+swaggerDocs(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
